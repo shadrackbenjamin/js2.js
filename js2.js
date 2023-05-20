2328d1411fbfe7e307a7fbe8f2d0a37a17ae6134
shadrackbenjamin <shadrackbenjamin99@gmail.com>
2023-05-20 14:53:25 +0000

    js2

---
+++ js2.js
@@ -0,0 +1,77 @@
+
+
+
+console.log(55 + 9);
+console.log(90+ "9");
+console.log(78 + 67);
+
+console.log(12.776 - 9.9);
+console.log(12.776 - "9.9");
+console.log(444 - "100");
+
+
+
+console.log(2 * 7.6);
+console.log(2 * "7.6");
+
+
+
+console.log(10 / 2.6);
+console.log("10" / 2.6);
+console.log("10" / 100)
+
+
+console.log(15 % 6);
+console.log(20 % 7);
+console.log(15 % "9");
+
+
+let num1 = 100;
+let num2 = 50;
+let num3 = 20
+console.log(++num1);
+console.log(num2++);
+console.log(--num3);
+
+
+
+let num5 = 70;
+let num6 = 35;
+let num7 = 80
+console.log(--num5);
+console.log(num6--);
+console.log(num7);
+
+
+
+
+console.log(12 == 45);
+console.log("12" == 12);
+console.log("kofi" == "kofi");
+
+
+console.log(12 === 13);
+console.log(13 === "12");
+console.log("female" === "male");
+
+
+console.log(100 != 50);
+console.log(60 != "70");
+console.log("maria" != "hello");
+
+
+console.log(60 !== 100);
+console.log(60 !== "27");
+console.log("Abu" !== "fada");
+
+
+console.log(17 > 13);
+console.log(100 > "13");
+console.log(60 > "13");
+console.log("159" > 13);
+
+let name = "shady";
+let num = 20;
+
+console.log("his name is " + name)
+console.log("he is  " + num )
